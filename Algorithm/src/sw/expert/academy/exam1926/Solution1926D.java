fedc374bb3394f713e1fc08d71dadf7007ce7418
shyunju7 <shyunju7@naver.com>
2022-07-08 19:38:21 +0900

    First Commit

---
+++ Algorithm/src/sw/expert/academy/exam1926/Solution1926D.java
@@ -0,0 +1,7 @@
+package sw.expert.academy.exam1926;
+/**
+ * 1926. 간단한 369게임 D2
+ * 이다은
+ * */
+public class Solution1926D {
+}
---
+++ Algorithm/src/sw/expert/academy/exam1926/Solution1926H.java
@@ -0,0 +1,7 @@
+package sw.expert.academy.exam1926;
+/**
+ * 1926. 간단한 369게임 D2
+ * 송현주
+ * */
+public class Solution1926H {
+}
