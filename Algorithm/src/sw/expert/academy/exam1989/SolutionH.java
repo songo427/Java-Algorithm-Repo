85410cdd04b61a1304f9dcc5c22db13e2e741788
HyunJu Song <38373150+shyunju7@users.noreply.github.com>
2022-07-27 14:56:41 +0900

    [SWEA] 1989H

---
+++ Algorithm/src/sw/expert/academy/exam1989/SolutionH.java
@@ -0,0 +1,36 @@
+package sw.expert.academy.exam1989;
+
+import java.util.Scanner;
+
+/**
+ * 1989. 초심자의 회문 검사 송현주
+ **/
+
+public class SolutionH {
+	public static void main(String args[]) {
+
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T = sc.nextInt();
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			StringBuilder input = new StringBuilder(sc.next());
+			int answer = -1;
+
+			StringBuilder reversedString = new StringBuilder(input);
+			reversedString = reversedString.reverse();
+
+			for (int i = 0; i < input.length(); i++) {
+				if (input.charAt(i) != reversedString.charAt(i)) {
+					answer = 0;
+					break;
+				} else {
+					answer = 1;
+					continue;
+				}
+			}
+			System.out.println("#" + test_case + " " + answer);
+		}
+		sc.close();
+	}
+}
