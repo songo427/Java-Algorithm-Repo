090f71fa14d4665cd9b954c5ce3d58406792d5ee
HyunJu Song <38373150+shyunju7@users.noreply.github.com>
2022-07-27 16:53:40 +0900

    [SWEA] 1946H

---
+++ Algorithm/src/sw/expert/academy/exam1946/SolutionH.java
@@ -0,0 +1,44 @@
+package com.algo.exam1946;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * 1946. 간단한 압축 풀기 
+ * 송현주
+ **/
+
+public class SolutionH {
+
+	public static void main(String args[]) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T = sc.nextInt();
+		for (int test_case = 1; test_case <= T; test_case++) {
+			StringBuilder answer = new StringBuilder();
+			int N = sc.nextInt();
+
+			for (int i = 0; i < N; i++) {
+				char key = sc.next().charAt(0);
+				int loop = sc.nextInt();
+
+				for (int k = 0; k < loop; k++) {
+					answer.append(key);
+				}
+			}
+
+			// print
+			System.out.println("#" + test_case);
+			for (int i = 0; i < answer.length();) {
+				if (i + 10 >= answer.length()) {
+					System.out.println(answer.substring(i));
+
+				} else {
+					System.out.println(answer.substring(i, i + 10));
+				}
+				i += 10;
+			}
+		}
+	}
+}
