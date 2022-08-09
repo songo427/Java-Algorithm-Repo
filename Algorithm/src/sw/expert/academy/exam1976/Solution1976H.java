1572df35d5ac4b5a7e78bca16d3d9a61ab97cb3e
shyunju7 <shyunju7@naver.com>
2022-08-09 20:40:53 +0900

    [SWEA] 1976H

--- Algorithm/src/sw/expert/academy/exam1976/Solution1976H.java
+++ Algorithm/src/sw/expert/academy/exam1976/Solution1976H.java
@@ -1,2 +1,47 @@
-package sw.expert.academy.exam1976;public class Solution1976H {
+package sw.expert.academy.exam1976;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 1976. 시각 덧셈 D2
+ * 송현주
+ */
+public class Solution1976H {
+    static int hour;
+    static int minute;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine().trim());
+
+        for (int testCase = 1; testCase <= T; testCase++) {
+            hour = 0;
+            minute = 0;
+
+            String input = br.readLine();
+            StringTokenizer tokenizer = new StringTokenizer(input, " ");
+            hour += Integer.parseInt(tokenizer.nextToken());
+            minute += Integer.parseInt(tokenizer.nextToken());
+            hour += Integer.parseInt(tokenizer.nextToken());
+            minute += Integer.parseInt(tokenizer.nextToken());
+            checkHourAndMinute();
+            System.out.println("#" + testCase + " " + hour + " " + minute);
+        }
+    }
+
+    private static void checkHourAndMinute() {
+        if (minute >= 60) {
+            minute -= 60;
+            hour += 1;
+        }
+
+        if (hour >= 13) {
+            hour -= 12;
+        }
+    }
+
 }
