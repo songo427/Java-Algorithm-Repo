4a95f7ac54bd8b2e23886a6ba110965a5806a2d3
shyunju7 <shyunju7@naver.com>
2022-08-12 22:05:46 +0900

    [BAEKJOON] 2606H

--- Algorithm/src/baekjoon/silver/exam2606/Solution2606H.java
+++ Algorithm/src/baekjoon/silver/exam2606/Solution2606H.java
@@ -1,2 +1,56 @@
-package baekjoon.silver.exam2606;public class Solution2606H {
+package baekjoon.silver.exam2606;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 2606. 바이러스
+ * 송현주
+ */
+public class Solution2606H {
+
+    static int N, computerCount;
+    static int[][] network;
+    static int answer;
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        computerCount = Integer.parseInt(br.readLine().trim());
+        N = Integer.parseInt(br.readLine().trim());
+        network = new int[computerCount + 1][computerCount + 1];
+        boolean[] visited = new boolean[computerCount+ 1];
+        answer = 0;
+
+        // input
+        for (int i = 1; i <= N; i++) {
+            String input = br.readLine();
+            StringTokenizer tokenizer = new StringTokenizer(input);
+            int x = Integer.parseInt(tokenizer.nextToken());
+            int y = Integer.parseInt(tokenizer.nextToken());
+            network[x][y] = 1;
+            network[y][x] = 1;
+        }
+
+        dfs(visited, 1);
+        System.out.print(answer);
+    }
+
+    private static void dfs(boolean[] visited, int v) {
+
+        visited[v] = true;
+
+        for (int i = 1; i <= computerCount; i++) {
+            if (!visited[i] && network[v][i] == 1) {
+                answer++;
+                dfs(visited, i);
+            }
+        }
+
+    }
 }
+
