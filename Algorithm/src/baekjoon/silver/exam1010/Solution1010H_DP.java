e8c2f384960b958a4c7e687f45ac2dfe15078475
shyunju7 <shyunju7@naver.com>
2022-08-17 00:36:29 +0900

    [BAEKJOON] 1010H_DP

--- Algorithm/src/baekjoon/silver/exam1010/Solution1010H_DP.java
+++ Algorithm/src/baekjoon/silver/exam1010/Solution1010H_DP.java
@@ -1,2 +1,39 @@
-package baekjoon.silver.exam1010;public class Solution1010H_DP {
+package baekjoon.silver.exam1010;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution1010H_DP {
+    static int R, N;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int testCase = 1; testCase <= T; testCase++) {
+            String line = br.readLine();
+            StringTokenizer tokenizer = new StringTokenizer(line);
+
+            R = Integer.parseInt(tokenizer.nextToken());  // N
+            N = Integer.parseInt(tokenizer.nextToken());  // M
+            dp = new int[N+1][R+1];
+            System.out.println(comb(N, R));
+        }
+    }
+
+    private static int comb(int n, int r) {
+        if (r == n || r == 0) {
+            return 1;
+        }
+
+        if (dp[n][r] != 0) {
+            return dp[n][r];
+        }
+
+        return dp[n][r] = comb(n - 1, r - 1) + comb(n - 1, r);
+    }
 }
