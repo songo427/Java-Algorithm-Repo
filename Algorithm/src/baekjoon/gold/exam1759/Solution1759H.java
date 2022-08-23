c9be3f395306fb4ff1c8a0b9353082237c1095bf
shyunju7 <shyunju7@naver.com>
2022-08-23 09:38:59 +0900

    Update exam1759/Soultuin1759H.java

--- Algorithm/src/baekjoon/gold/exam1759/Solution1759H.java
+++ Algorithm/src/baekjoon/gold/exam1759/Solution1759H.java
@@ -18,31 +18,38 @@ public class Solution1759H {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        answer = new StringBuilder();
+
+        // 입력
         String input = br.readLine();
         StringTokenizer st = new StringTokenizer(input, " ");
-
-        answer = new StringBuilder();
         L = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
         password = new char[L];
         input = br.readLine();
 
+        // 알파벳 저장
         String[] tmp = input.split(" ");
         alphabet = new char[C];
-
         for (int i = 0; i < C; i++) {
             alphabet[i] = tmp[i].charAt(0);
         }
 
+        // 사전순
         Arrays.sort(alphabet);
-        System.out.println(Arrays.toString(alphabet));
 
+        // 조합
         comb(0, 0);
+
+        // 출력
         System.out.print(answer.toString().trim());
     }
 
+    // 조합
     private static void comb(int start, int cnt) {
         if (L == cnt) {
+
+            // 크기가 L인 조합이 완성되면 암호 가능성 체크
             isPassword(password);
             return;
         }
@@ -53,6 +60,7 @@ public class Solution1759H {
         }
     }
 
+    // 암호 가능성 체크
     private static void isPassword(char[] password) {
         String tmp = "";
         int vowels = 0;
@@ -63,6 +71,7 @@ public class Solution1759H {
             tmp += password[i];
         }
 
+        // 가능한 암호라면, answer에 저장
         if (vowels > 0 && L - vowels >= 2) {
             answer.append(tmp + "\n");
         }
