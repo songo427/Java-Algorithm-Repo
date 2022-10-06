db786c0e6bfb5970cf3951efe5e4959853a86c86
shyunju7 <shyunju7@naver.com>
2022-10-06 22:54:53 +0900

    [BAEKJOON] 13023H

--- Algorithm/src/baekjoon/gold/exam13023/Solution13023H.java
+++ Algorithm/src/baekjoon/gold/exam13023/Solution13023H.java
@@ -13,45 +13,59 @@ public class Solution13023H {
     static int N, M;
     static List<List<Integer>> friends;
     static boolean[] visited;
+    static boolean answer;
 
     public static void main(String[] args) throws IOException {
 
+        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
         StringTokenizer tokenizer = new StringTokenizer(input, " ");
         N = Integer.parseInt(tokenizer.nextToken());
         M = Integer.parseInt(tokenizer.nextToken());
-
-        // 친구 수만큼
-//        friends = new int[N][N];
         friends = new ArrayList<>();
-        visited = new boolean[N];
 
+        // 사람 수만큼 생성
+        for (int i = 0; i < N; i++) {
+            friends.add(new ArrayList<>());
+        }
+
+        // 친구 관계 저장
         for (int i = 0; i < M; i++) {
             input = br.readLine();
             tokenizer = new StringTokenizer(input, " ");
             int friendA = Integer.parseInt(tokenizer.nextToken());
             int friendB = Integer.parseInt(tokenizer.nextToken());
 
+            // 서로 친구 관계
+            friends.get(friendA).add(friendB);
+            friends.get(friendB).add(friendA);
         }
 
-        int answer = 0;
-        if (dfs(0, 0)) {
-            answer = 1;
+        // 모든 범위 탐색
+        for (int i = 0; i < N; i++) {
+            if(answer) break;
+            visited = new boolean[N];
+            dfs(i, 0);
         }
-        System.out.print(answer);
+        System.out.println(answer? 1 : 0);
     }
 
-    private static boolean dfs(int v, int cnt) {
+    // dfs 탐색
+    private static void dfs(int current, int cnt) {
 
         if (cnt == 4) {
-            return true;
+            answer = true;
+            return;
         }
-        visited[v] = true;
-        for (int i = 0; i < N; i++) {
+
+        visited[current] = true;
+
+        for (int v : friends.get(current)) {
+            if (visited[v]) continue;
+            dfs(v, cnt + 1);
         }
-        visited[v] = false;
-        return false;
+        visited[current] = false;
     }
 }
 
